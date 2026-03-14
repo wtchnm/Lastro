4deed241028e2c2feff4a1986ae053340ed0d74f
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2026-03-13 21:27:27 -0300

    feat: upgrade to Astro 6

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({ vite: { plugins: [tailwindcss()] } })
+export default defineConfig({
+	security: { csp: true },
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: 'Inter',
+			cssVariable: '--font-inter',
+			weights: ['100 900']
+		}
+	],
+	vite: { plugins: [tailwindcss()] }
+})
